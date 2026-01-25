ca2a2ed8faf0b606a6320ea006eb43a9178d723d
Satvik <satvikbharti318@gmail.com>
2026-01-25 16:42:14 +0530

    Initial commit

---
+++ server/inngest/index.js
@@ -0,0 +1,73 @@
+import { Inngest } from "inngest";
+import User from "../modals/user.js";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "pingup-app" });
+
+// Inngest function to save user data to a database
+const syncUserCreation = inngest.createFunction(
+    { id: 'sync-user-from-clerk' },
+    { event: 'clerk/user.created' },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_address, image_url } = event.data
+        let username = email_address[0].email_address.split('@')[0]
+
+        // Check avability of user
+        const user = await User.findOne({ username })
+
+        if (user) {
+            username = username + Math.floor(Math.random() * 10000)
+        }
+
+        const userData = {
+            _id: id,
+            email: email_address[0].email_address,
+            full_name: first_name + "" + last_name,
+            profile_picture: image_url,
+            username
+        }
+        await User.create(userData)
+    }
+)
+
+
+// Inngest Function to update data in datbase
+
+const syncUserUpdation = inngest.createFunction(
+    { id: 'update-user-from-clerk' },
+    { event: 'clerk/user.updated' },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_address, image_url } = event.data
+       
+
+        // Check avability of user
+        const user = await User.findOne({ username })
+
+        const updatedUserData= {
+            email: email_address[0].email_address,
+            full_name:first_name + ' ' + last_name,
+            profile_picture:image_url
+        }
+   
+        await User.findByIdAndUpdate(id , updatedUserData)
+    }
+)
+
+// Inngest Function to delete user from datbase
+
+const syncUserDeletion = inngest.createFunction(
+    { id: 'delete-user-from-clerk' },
+    { event: 'clerk/user.deleted' },
+    async ({ event }) => {
+        const { id } = event.data
+       await User.findByIdAndDelete(id)
+    }
+)
+
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion
+];
