71a6a062e8b224e396ab3345d95d1cb95af0954f
Satvik <satvikbharti318@gmail.com>
2026-01-30 23:32:23 +0530

    error solved2

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,11 +1,10 @@
-import ImageKit from 'imagekit';
+import ImageKit from "@imagekit/nodejs"
 
-var imageKit = new ImageKit({
-    publicKey: process.env.IMAGEKIT_PUBLIC_KEY ,
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY ,
-    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
-});
-
+const imageKit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
+})
 
 export default imageKit
 
