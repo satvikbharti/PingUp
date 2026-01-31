20499d66bcc618843f1164459b156e63c8cbd33e
Satvik <satvikbharti318@gmail.com>
2026-01-31 16:02:39 +0530

    api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import 'dotenv/config';
 import connectDB from './configs/db.js';
 import { inngest, functions } from "./inngest/index.js"
-import { serve } from 'inngest/express';
+import { serve } from "inngest/express";
 import { clerkMiddleware } from '@clerk/express'
 import userRouter from './routes/userRoutes.js';
 
@@ -14,13 +14,8 @@ await connectDB()
 
 app.use(express.json())
 app.use(cors())
-app.use(clerkMiddleware())
+app.use("/api/user", clerkMiddleware())
 
-app.get("/debug-headers", (req, res) => {
-  res.json({
-    authHeader: req.headers.authorization || null
-  })
-})
 
 app.get('/' , (req , res)=> res.send('Server is running'))
 app.use('/api/inngest', serve({ client: inngest, functions }))
