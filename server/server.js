1f3226fed6403661e1821d7b48577a7c9d5b478d
Satvik <satvikbharti318@gmail.com>
2026-02-02 01:54:25 +0530

    backeb

--- server/server.js
+++ server/server.js
@@ -14,12 +14,13 @@ import messageRouter from './routes/messageRoutes.js';
 const app = express()
 
 await connectDB()
-app.use('/api/inngest', serve({ client: inngest, functions }))
+
 app.use(express.json())
 app.use(cors())
 app.use(clerkMiddleware())
 
 app.get('/' , (req , res)=> res.send('Server is running'))
+app.use('/api/inngest', serve({ client: inngest, functions }))
 app.use('/api/user', userRouter)
 app.use('/api/post', postRouter)
 app.use('/api/story', storyRouter)
